90726cd82b37a618e9e27332fb2fe0b4fe8a7ae8
yraja963 <yraja@gmail.com>
2020-11-28 15:15:00 +0530

    answer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 var http = require("http");
+const { runInNewContext } = require("vm");
 
 const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+    if(req.url.startWith === "/welcome" && req.method === "GET"){
+        res.writeHead(200,{"content-type":"text/plain",});
+        res.write("Welcome to Dominos!");
+        res.end();
+    }
+    else if(req.url.startWith === "/contact" && req.method === "GET"){
+        res.writeHead(200,{"content-type":"application/json",});
+        res.write(JSON.stringify({phone:"18602100000",email:"guestcaredominos@jublfood.com"}));
+        res.end();
+    } else{
+        res.writeHead(404,{});
+        res.end();
+    }
   
 }
+httpServer.listen(8081);
 
 module.exports = httpServer;
